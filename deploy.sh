da71227ed83c1c4489a3fe1349289412100f6fca
Automation <automation@example.com>
2025-08-10 07:38:30 +0400

    deploy 1

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,7 @@ IMAGE="ghcr.io/evolvus/github-smart:latest"
 APP_NAME="github-smart-app"
 MYSQL_NAME="github-smart-mysql"
 
-if [ -n "${GHCR_TOKEN:-${GITHUB_TOKEN:-}}" ]; then
-  echo "Logging into ghcr.io with provided token..."
-  echo "${GHCR_TOKEN:-${GITHUB_TOKEN:-}}" | docker login ghcr.io -u "${GHCR_USER:-"$USER"}" --password-stdin
-else
-  echo "Public package: skipping ghcr.io login"
-fi
+
 
 echo "Pulling image $IMAGE..."
 docker pull "$IMAGE"
